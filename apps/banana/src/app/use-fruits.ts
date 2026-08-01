d7050c9fb61ff68990912a2810b6828b70b9e14a
tungdd98 <tungdd98@gmail.com>
2026-08-01 17:03:38 +0700

    style(banana): apply prettier trailing comma

--- apps/banana/src/app/use-fruits.ts
+++ apps/banana/src/app/use-fruits.ts
@@ -4,6 +4,6 @@ import { Fruit, FRUITS } from './fruit';
 export function useFruits(status?: Fruit['status']) {
   return useMemo(
     () => (status ? FRUITS.filter((f) => f.status === status) : FRUITS),
-    [status]
+    [status],
   );
 }
